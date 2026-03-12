f340e9c6e7ea2af5b3a86a4cce8a4a3f20352a6c
camil <camille.sleiman@student.hu.nl>
2026-03-13 00:06:25 +0100

    Add - Edit - Delete Medication

--- src/MedicatieOpslag.java
+++ src/MedicatieOpslag.java
@@ -1,23 +1,23 @@
-//import java.util.HashMap;
-//
-//public class MedicatieOpslag {
-//
-//    private final HashMap<String, Integer> medicaties = new HashMap<>();
-//
-//    public MedicatieOpslag() {
-//        medicaties.put("paracetamol", 5000);
-//        medicaties.put("ibuprofen", 5000);
-//        medicaties.put("antibiotica", 5000);
-//    }
-//
-//    public HashMap<String, Integer> getMedicaties() {
-//        return medicaties;
-//    }
-//
-//    public void toonMedicaties() {
-//        System.out.println("Medicatie opslag:");
-//        for (var entry : medicaties.entrySet()) {
-//            System.out.println("\n " + entry.getKey() + " = " + entry.getValue() + " mg");
-//        }
-//    }
-//}
+public class MedicatieOpslag {
+
+    private final String name;
+    private String dosage;
+
+    public MedicatieOpslag(String name, String dosage) {
+        this.name = name;
+        this.dosage = dosage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | Dosage: " + dosage;
+    }
+}
--- src/Patient.java
+++ src/Patient.java
@@ -1,49 +1,41 @@
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 class Patient {
-   static final int RETURN      = 0;
-   static final int SURNAME     = 1;
-   static final int FIRSTNAME   = 2;
-   static final int DATEOFBIRTH = 3;
-   static final int AGE         = 4;
-   static final int WEIGHT      = 5;
-   static final int HEIGHT      = 6;
-   static final int BMI         = 7;
-   static final int MEDICATIE = 8;
+    static final int RETURN = 0;
+    static final int SURNAME = 1;
+    static final int FIRSTNAME = 2;
+    static final int DATEOFBIRTH = 3;
+    static final int AGE = 4;
+    static final int WEIGHT = 5;
+    static final int HEIGHT = 6;
+    static final int BMI = 7;
+    static final int MEDICATIE = 8;
 
-   int       id;
-   String    surname;
-   String    firstName;
-   LocalDate dateOfBirth;
-   int       age;
-   double    weight;
-   double    height;
-   double    bmi;
-   String medication;
+    int id;
+    String surname;
+    String firstName;
+    LocalDate dateOfBirth;
+    int age;
+    double weight;
+    double height;
+    double bmi;
 
+    public final List<MedicatieOpslag> medicatie = new ArrayList<>();
 
-
-
-    /**
-     * Constructor
-     */
     Patient(int id, String surname, String firstName, LocalDate dateOfBirth, double weight, double height) {
         this.id = id;
         this.surname = surname;
         this.firstName = firstName;
         this.dateOfBirth = dateOfBirth;
         this.weight = weight;
-        this.height =  height;
+        this.height = height;
 
-
-
-
-        //leeftijd berekening
         this.age = Period.between(dateOfBirth, LocalDate.now()).getYears();
 
-        //Bmi berekening, BMI = gewicht / lengte * lengte
         if (height > 0) {
             this.bmi = weight / (height * height);
         } else {
@@ -59,19 +51,24 @@ class Patient {
         return firstName;
     }
 
-    int getAge() {return age;}
+    int getAge() {
+        return age;
+    }
 
-    double getWeight() {return weight;}
-    double getHeight() {return height;}
+    double getWeight() {
+        return weight;
+    }
+
+    double getHeight() {
+        return height;
+    }
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    double getBmi() {return bmi;}
+    double getBmi() {
+        return bmi;
+    }
 
-
-    /**
-     * Display patient data.
-     */
     void viewData() {
         System.out.format("===== Patient id=%d ==============================\n", id);
         System.out.format("%-17s %s\n", "Surname:", surname);
@@ -81,16 +78,43 @@ class Patient {
         System.out.format("%-17s %s\n", "Weight:", weight + " kg");
         System.out.format("%-17s %s\n", "Height:", height + " m");
         System.out.format("%-17s %s\n", "BMI:", bmi);
-        System.out.format("%-17s %s\n", "Medication:", medication);
+
+        if (medicatie.isEmpty()) {
+            System.out.format("%-17s %s\n", "Medication:", "Geen medicatie");
+        } else {
+            System.out.println("Medication:");
+            for (MedicatieOpslag m : medicatie) {
+                System.out.println("- " + m);
+            }
+        }
     }
 
+    public void addMedication(String medication, String dosage) {
+        medicatie.add(new MedicatieOpslag(medication, dosage));
+    }
 
-    /**
-     * Shorthand for a Patient's full name
-     */
+    public void updateMedicationDosage(String medication, String dosage) {
+        for (MedicatieOpslag m : medicatie) {
+            if (m.getName().equalsIgnoreCase(medication)) {
+                m.setDosage(dosage);
+                System.out.println("Dosage updated for " + medication);
+                return;
+            }
+        }
+        System.out.println("Medication not found");
+    }
+
+    public void deleteMedication(String medication) {
+        for (MedicatieOpslag m : medicatie) {
+            if (m.getName().equalsIgnoreCase(medication)) {
+                medicatie.remove(m);
+                System.out.println("Dosage deleted from " + medication);
+                return;
+            }
+        }
+    }
+
     String fullName() {
         return String.format("%s %s [%d]", firstName, surname, age);
     }
-
-
 }
